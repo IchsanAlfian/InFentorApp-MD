58b8667fa1e3a8dce69f4389e8242fce2eb9c1b3
IchsanAlfian <eksanalfian@gmail.com>
2023-06-08 13:30:07 +0700

    add login regist

---
+++ app/src/main/java/com/capstone/infentor/ui/LoginActivity.kt
@@ -0,0 +1,24 @@
+package com.capstone.infentor.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.infentor.R
+import com.capstone.infentor.databinding.ActivityEssayBinding
+import com.capstone.infentor.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+
+        binding.tvSignup.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/capstone/infentor/ui/SignUpActivity.kt
+++ app/src/main/java/com/capstone/infentor/ui/SignUpActivity.kt
@@ -3,17 +3,20 @@ package com.capstone.infentor.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.capstone.infentor.databinding.ActivityMainBinding
+import com.capstone.infentor.R
+import com.capstone.infentor.databinding.ActivityLoginBinding
+import com.capstone.infentor.databinding.ActivitySignUpBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
-        binding.btnPilgan.setOnClickListener {
+
+
+        binding.tvLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
